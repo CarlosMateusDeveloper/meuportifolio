882b37dc575e0de82fd82342cb09b7865d083c4a
Carlos Mateus <mateusdev33@gmail.com>
2025-10-22 22:05:16 -0300

    inicio do footer

---
+++ site2/src/components/footer/footer.jsx
@@ -0,0 +1,28 @@
+import './footer.css'
+export default function Footer(){
+    return(
+        <section>
+            <div>
+                <div>
+                    <i className='ri-building-3-line text-danger'></i> <h5>Construct</h5>
+                    <p>We build security and trust in homes.</p>
+                    <p>Email: construct123@gmail.com</p>
+                </div>
+                <div>
+                    <h5>Company</h5>
+                    <a href="">About Us</a>
+                    <a href="">Services</a>
+                    <a href="">Projects</a>
+                </div>
+                <div>
+                    <h5>Information</h5>
+
+                </div>
+                <div>
+                    <h5>Social Media</h5>
+                </div>
+            </div>
+            <p>All right reserved By Mateus.code</p>
+        </section>
+    )
+}
