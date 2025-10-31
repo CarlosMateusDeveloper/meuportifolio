b2cdb5909a0cc96b4347de910af63951a226c5a2
Carlos Mateus <mateusdev33@gmail.com>
2025-10-30 22:41:16 -0300

    ajustes na responsividade e em about

--- portifolio/src/componentes/about/about.jsx
+++ portifolio/src/componentes/about/about.jsx
@@ -2,30 +2,36 @@ import './about.css'
 import foto from '../../assets/fotoportifolio-1.png'
 import foto2 from '../../assets/foto2.png'
 import fotoesportes from '../../assets/foto-esportes.jpg'
-import FloatingBubbles from "../bubble/bubble";
 export default function About() {
     return (
-        <section className="about pt-5 pb-5" id='about'>
+        <section className="about py-5 container-fluid" id='about'>
             <h1>Sobre mim</h1>
-            <div className='d-flex flex-row'>
-                <div className="content-text">
-                <p className="about-text text-white pt-5 px-4 align-content-center">
-                Sou Desenvolvedor Fullstack em formação no Instituto Federal do Rio Grande do Norte (IFRN), campus Parnamirim. Atualmente, curso Tecnologia em Sistemas para Internet e estou no 4º semestre, com previsão de conclusão em dezembro de 2027.
+            <div className='d-flex flex-row mx-5'>
+                <div className="content-text w-75">
+                    <p className="about-text text-white pt-5 px-4 align-content-center fs-4">
+                        Sou Desenvolvedor Fullstack em formação no Instituto Federal do Rio Grande do Norte (IFRN), campus Parnamirim.      Atualmente, curso Tecnologia em Sistemas para Internet e estou no 4º semestre, com previsão de conclusão em dezembro de 2027.
+                    </p>
+                    <br />
+                    <p className='about-text text-white pt-5 px-4 align-content-center fs-4'>
+                        Trabalho com HTML, CSS, JavaScript/TypeScript, uso frameworks como Angular e React (este portfólio foi desenvolvido em React).
+                        Uso PostgreSQL e mySQL como banco de dados e no Backend utilizo nodeJS ou Python. Gosto de aprender sobre Cloud Computing, IA e Blockchain.
+                    </p>
+                    <br />
+                    <p className='about-text text-white pt-5 px-4 align-content-center fs-4'>
+                        Como Hobbies eu procuro ser bem ativo fisicamente, procuro me distanciar de telas e coisas como jogos e redes sociais, busco fazer algo que ocupe a minha mente e gaste as energias do meu corpo como ir à academia, praticar Jiu-jitsu, caminhar, estou tentando pegar o hábito de ler um livro e pelo menos 10 páginas por dia.
+                    </p>
+                        
                 
-                <br />
-                  Trabalho com HTML, CSS, JavaScript/TypeScript, uso frameworks como Angular e React (este portfólio foi desenvolvido em React).
-                    Uso PostgreSQL e mySQL como banco de dados e no Backend utilizo nodeJS ou Python. Gosto de aprender sobre Cloud Computing, IA e Blockchain.
-                
-                <br />
+                       
             
-                    Como Hobbies eu procuro ser bem ativo fisicamente, procuro me distanciar de telas e coisas como jogos e redes sociais, busco fazer algo que ocupe a minha mente e gaste as energias do meu corpo como ir à academia, praticar Jiu-jitsu, caminhar, estou tentando pegar o hábito de ler um livro e pelo menos 10 páginas por dia.
-                </p>
-            </div>
-            <div className="content-image">
-                <img className='rounded-circle' src={foto} alt="" />
-                <img className=' rounded-circle' src={foto2} alt="" />
-                <img className='rounded-circle me-3' src={fotoesportes} alt="" />
-            </div>
+                        
+                     
+                </div>
+                <div className="content-image">
+                    <img className='rounded-circle animate-float img-1' src={foto} alt="" />
+                    <img className=' rounded-circle animate-float img-2' src={foto2} alt="" />
+                    <img className='rounded-circle me-3 animate-float img-3' src={fotoesportes} alt="" />
+                </div>
             </div>
             
            
