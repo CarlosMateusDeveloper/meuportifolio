b3e18fbc1f06833782663e2d1af951b26f3ea5b7
Carlos Mateus <mateusdev33@gmail.com>
2025-10-31 21:38:49 -0300

    ajustes

--- portifolio/src/componentes/maindiv/Maindiv.jsx
+++ portifolio/src/componentes/maindiv/Maindiv.jsx
@@ -2,17 +2,17 @@ import './Maindiv.css'
 import perfil from '../../assets/fotoportifolio.jpg'
 export default function Maindiv() {
   return ( 
-  <section className="home d-flex flex-row align-itens-center justify-content-center container-fluid px-5" id="home">       
-        <div className="home-content d-fex flex-column align-itens-center justify-content-center mt-5  px-5">
+  <section className="home d-flex flex-row align-items-center justify-content-center container-fluid px-5 " id="home">       
+        <div className="home-content d-fex flex-column justify-content-center mb-5 px-5 ">
          <h1 className='text-end'><span>Mateus Gonçalves</span> </h1>  
          <h3 className="typing-text text-end">Desenvolvedor de Software</h3>
-         <p className='text-white w-5 fs-5 text-end ms-auto'>Desenvolvimento de soluções de software de ponta a ponta, com foco em performance, escalabilidade e design intuitivo. Especialista em React, Node.js e python  para criar aplicações web robustas que impulsionam resultados.</p>
-         <div className="social-icons d-flex justify-content-end gap-3 fs-3">
+         <p className='text-secondary fs-5 text-end ms-auto'>Desenvolvimento de soluções de software de ponta a ponta, com foco em performance, escalabilidade e design intuitivo. Especialista em React, Node.js e python  para criar aplicações web robustas que impulsionam resultados.</p>
+         <div className="social-icons d-flex justify-content-end gap-3 fs-3 mb-5">
             <a className=' d-inline-flex justify-content-center align-items-center border rounded-circle border-danger' href="https://www.linkedin.com/in/carlos-gonçalves-b38701211/" target="_blank"><i class="bi bi-linkedin text-danger w-50"></i></a>
             <a className='d-inline-flex justify-content-center align-items-center border rounded-circle border-danger ' href="https://github.com/CarlosMateusDeveloper" target="_blank"><i class="bi bi-github text-danger"></i></a>
             <a className='d-inline-flex justify-content-center align-items-center border rounded-circle border-danger ' href="https://www.instagram.com/dev_carlos_mateus" target="_blank"><i class="bi bi-instagram text-danger btn-hover"></i></a>
           </  div> 
         </div>
-        <div className="home-img me-5 pe-5 mt-4"> <img className='rounded-circle' src={perfil} alt=""/></div>
+        <div className="home-img me-5 pe-5 mb-5 "> <img className='rounded-circle' src={perfil} alt=""/></div>
       </section>)
 }
--- portifolio/src/componentes/navbar/navbar.jsx
+++ portifolio/src/componentes/navbar/navbar.jsx
@@ -16,9 +16,10 @@ export default function Navbar() {
               <img src="/ai-svgrepo-com.svg" alt="" />
             </Nav.Link>
             <Nav.Link as={HashLink} to="/#about">Sobre mim</Nav.Link>
+            <Nav.Link as={HashLink} to="/#Stacks">Stacks</Nav.Link>
             <Nav.Link as={HashLink} to="/#projects">Projetos</Nav.Link>
             <Nav.Link as={HashLink} to="/#contact">Contato</Nav.Link>
-            <Nav.Link as={HashLink} to="/#Stacks">Stacks</Nav.Link>
+            
           </Nav>
         </BootstrapNavbar.Collapse>
       </Container>
