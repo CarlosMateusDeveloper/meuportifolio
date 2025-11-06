9bc932e2ac6b04dd9902709e8e8d09b89f836e36
Carlos Mateus <mateusdev33@gmail.com>
2025-11-06 10:01:24 -0300

    trabalhando em stack

--- portifolio/src/componentes/stacks-content/tool.jsx
+++ portifolio/src/componentes/stacks-content/tool.jsx
@@ -1,10 +1,13 @@
 import './tool.css'
 export default function Tool({ icon , title, tech }){
     return(
-        <div className='tool'>
-          <div>{icon}</div>
-          <h1>{title}</h1>
-          {tech}
+        <div className='tool p-4'>
+          <div className="d-flex ">
+            <div className='stack-icon rounded-circle text-white d-flex align-items-end'>{icon}</div>
+            <h1 className='text-white d-flex align-items-center '>{title}</h1>
+          </div>
+          
+          <div className='d-flex'>{tech}</div>
         </div>
     )   
 }
